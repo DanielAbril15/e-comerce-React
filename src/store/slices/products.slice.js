e73c9651cd4657e9333e666d04134cce360ce042
Daniel Abril <danbril1015@gmail.com>
2022-12-23 18:06:47 -0500

    cambio

--- src/store/slices/products.slice.js
+++ src/store/slices/products.slice.js
@@ -13,7 +13,7 @@ export const { setProducts } = productsSlice.actions;
 export default productsSlice.reducer;
 
 export const getAllProducts = () => (dispatch) => {
-  const URL = "https://ecommerce-api-react.herokuapp.com/api/v1/products";
+  const URL = "https://e-commerce-api.academlo.tech/api/v1/products";
   return axios
     .get(URL)
     .then((res) => dispatch(setProducts(res.data.data.products)))
